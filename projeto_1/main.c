6c62400564376e4b5b57cb9e57d2861f306179b9
RCristyan <rcristyan9@gmail.com>
2021-03-12 19:38:52 -0300

    Leituras dos sensores mostradas no LCD.

--- projeto_1/main.c
+++ projeto_1/main.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "uart_modbus.h"
 #include "i2c_read_bme280.h"
+#include "i2c_lcd_write.h"
 
 int main(){
     configura_uart();
-
-    printf("Temperatura fornecida pelo sensor LM35 = %.2f °C\n", get_LM35_reading());
-    printf("-----------\n");
-    printf("Temperatura fornecida pelo potenciometro = %.2f °C\n", get_potenciometro_reading());
-
     setupBME280();    
-    printf("Valores fornecidos pelo BME:\n");
-    printf("T = %2f °C, P = %6.2f Pa, H = %2.2f%%\n", get_BME280_reading('t'), get_BME280_reading('p'), get_BME280_reading('u'));
+    configura_i2c_lcd();
+
+    float TI, TE, TR;
+    float p, u;
+
+    while (1)   {
+        TI = get_LM35_reading();
+        TE = get_BME280_reading('t');
+        p = get_BME280_reading('p');
+        u = get_BME280_reading('u');
+        TR = get_potenciometro_reading();
+
+        char ti_buffer[20];
+        char te_buffer[20];
+        char tr_buffer[20];
+
+        gcvt(TI, 4, ti_buffer);
+        gcvt(TE, 4, te_buffer);
+        gcvt(TR, 4, tr_buffer);
+
+        printf("TI (buffer) = ");
+        printf("%s, ", ti_buffer);
+        printf("TE (buffer) = ");
+        printf("%s, ", te_buffer);
+        printf("TR (buffer) = ");
+        printf("%s\n", tr_buffer);
+
+        char line1[50];
+        char line2[50];
+        strcpy(line1, "TI:");
+        strcat(line1, ti_buffer);
+        strcat(line1, " TR:");
+        strcat(line1, tr_buffer);
+
+        strcpy(line2, "TE:");
+        strcat(line2, te_buffer);
+
+        clear_lcd();
+        lcd_write(LINE1, line1, 0);
+        lcd_write(LINE2, line2, 1000);
+    }
 
     close_uart();
 
