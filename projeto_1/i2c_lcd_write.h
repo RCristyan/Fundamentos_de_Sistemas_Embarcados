4a863dc081cb63c58cfcfd24c0806574bb409740
RCristyan <rcristyan9@gmail.com>
2021-03-12 19:28:14 -0300

    Adicionada escrita em LCD.

---
+++ projeto_1/i2c_lcd_write.h
@@ -0,0 +1,11 @@
+#ifndef __I2C_LCD_WRITE__
+#define __I2C_LCD_WRITE__
+
+#define LINE1  0x80 // 1st line
+#define LINE2  0xC0 // 2nd line
+
+void configura_i2c_lcd();
+void lcd_write(int line, char string[20], int delay_ms);
+void clear_lcd();
+
+#endif
